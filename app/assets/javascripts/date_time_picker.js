70d3afe4fb2022147ac25028aa9a6c1d7a286699
Claire Guillou <guillou.clr@gmail.com>
2021-09-08 17:06:55 +0200

    carte ok

--- app/assets/javascripts/date_time_picker.js
+++ app/assets/javascripts/date_time_picker.js
@@ -6,25 +6,3 @@ $( document ).on('turbolinks:load', function() {
   });
 });
 
-
-// var initialText = $('.editable').val();
-// $('.editOption').val(initialText);
-
-// $('#test').change(function(){
-// var selected = $('option:selected', this).attr('class');
-// var optionText = $('.editable').text();
-
-// if(selected == "editable"){
-//   $('.editOption').show();
-
-
-//   $('.editOption').keyup(function(){
-//       var editText = $('.editOption').val();
-//       $('.editable').val(editText);
-//       $('.editable').html(editText);
-//   });
-
-// }else{
-//   $('.editOption').hide();
-// }
-// });
